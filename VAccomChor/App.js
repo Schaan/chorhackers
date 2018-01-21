db335e19d40073e5811ebc3362370f7b71862554
Schaan <huas2005@gmail.com>
2018-01-21 23:29:06 +0100

    Set app with file system and web audio APIs: - installed fs and web-audio package - configured fs settings in android folder - a test app.js with file loading button
    !!!TO RUN this version:
    - run `npm install` first to install all new packages acc. packages.json
    - run `react-native run-android` with admin-right!!! (since it tries to
    visit the android directory of virtural device)

--- VAccomChor/App.js
+++ VAccomChor/App.js
@@ -1,36 +1,74 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
+ * Main app js for React app
+ * version: wip
+ * subject: built-in audio player with selecting local files
  */
+ // TODO: to write inline comments for App class !!!
+
+'use strict';
 
 import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
   Text,
+  Button,
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+var RNFS = require('react-native-fs');
+
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' +
+//     'Cmd+D or shake for dev menu',
+//   android: 'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
 
 export default class App extends Component<{}> {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      fileName : 'No selected'
+    };
+  }
+
+  loadAudio = () => {
+    var self = this;
+    RNFS.readDir(RNFS.DocumentDirectoryPath)
+      .then(function(res) {
+        return Promise.all([RNFS.stat(res[0].path), res[0].path]);
+      }).then((statResult) => {
+        if (statResult[0].isFile()) {
+          return RNFS.readFile(statResult[1], 'utf8');
+        }
+        self.setState(() => {
+          return {fileName : 'No file'};
+        });
+      })
+      .then((contents) => {
+        self.setState(() => {
+          return {fileName : 'There is a file'};
+        });
+      });
+      //
+    }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to our exciting ChorHacker Project!
         </Text>
+        <Button
+          onPress = {this.loadAudio}
+          title="Load Music"
+          color="#814584"
+          accessibilityLabel="Choose an audio file"
+        />
         <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
+          Load a music file: {this.state.fileName}
         </Text>
       </View>
     );
